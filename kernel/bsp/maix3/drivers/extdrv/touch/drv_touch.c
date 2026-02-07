41b7832b10f24c2e95baf1cb62c1714981bddc19
xelll <xel1995@163.com>
2026-02-07 21:42:59 +0800

    fix touch irq

--- kernel/bsp/maix3/drivers/extdrv/touch/drv_touch.c
+++ kernel/bsp/maix3/drivers/extdrv/touch/drv_touch.c
@@ -479,9 +479,9 @@ static rt_err_t drv_touch_control(struct rt_touch_device* touch, int cmd, void* 
             return RT_EINVAL;
         }
 
-        if (0x00 == dev->pin.fake_intr) {
-            kd_pin_irq_enable(dev->pin.intr, KD_GPIO_IRQ_ENABLE);
-        }
+        // if (0x00 == dev->pin.fake_intr) {
+        //     kd_pin_irq_enable(dev->pin.intr, KD_GPIO_IRQ_ENABLE);
+        // }
 
         return RT_EOK;
     } break;
@@ -490,9 +490,9 @@ static rt_err_t drv_touch_control(struct rt_touch_device* touch, int cmd, void* 
             return RT_EINVAL;
         }
 
-        if (0x00 == dev->pin.fake_intr) {
-            kd_pin_detach_irq(dev->pin.intr);
-        }
+        // if (0x00 == dev->pin.fake_intr) {
+        //     kd_pin_irq_enable(dev->pin.intr, KD_GPIO_IRQ_DISABLE);
+        // }
 
         return RT_EOK;
     } break;
